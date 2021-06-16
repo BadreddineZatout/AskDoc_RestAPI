cd428c79b19c4ffbf634e00df5f735e3e4bb433d
BadreddineZatout <hb_zatout@esi.dz>
2021-06-16 16:40:03 +0100

    doc controller

---
+++ App/controllers/Doctor.controller.js
@@ -0,0 +1,36 @@
+const db = require("../models");
+const Doctor = db.doctors;
+
+exports.findAuth = (req, res) => {
+    const tel = req.body.tel;
+    const pdw = req.body.pdw;
+    Doctor.findAll({
+        where: {
+            tel: tel,
+            pdw: pdw
+        }
+     })
+      .then(data => {
+        res.send(data);
+      })
+      .catch(err => {
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while retrieving tutorials."
+        });
+      });
+  };
+
+  exports.findOne = (req, res) => {
+    const id = req.params.id;
+  
+    Doctor.findByPk(id)
+      .then(data => {
+        res.send(data);
+      })
+      .catch(err => {
+        res.status(500).send({
+          message: "Error retrieving Tutorial with id=" + id
+        });
+      });
+  };
