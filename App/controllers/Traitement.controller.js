84ca0de810d34956fcae866318031eec68e85d67
BadreddineZatout <hb_zatout@esi.dz>
2021-06-17 15:33:14 +0100

    create traitement with post request

--- App/controllers/Traitement.controller.js
+++ App/controllers/Traitement.controller.js
@@ -1,6 +1,28 @@
 const db = require("../models");
 const Traitement = db.traitements;
 
+exports.create = (req, res) => {
+  // Validate request
+  if (!req.body.name) {
+    res.status(400).send({
+      message: "Content can not be empty!"
+    });
+    return;
+  }
+
+  // Save Doctor in the database
+  Traitement.create(req.body)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while creating the Tutorial."
+      });
+    });
+};
+
   exports.findAll = (req, res) => { 
     const id = req.params.id;
 
--- App/routes/Traitement.routes.js
+++ App/routes/Traitement.routes.js
@@ -2,6 +2,7 @@ module.exports = app =>{
     const traitement = require("../controllers/Traitement.controller");
     var router = require("express").Router();
 
+    router.post("/", traitement.create);
     router.get("/:id", traitement.findAll);
 
     app.use('/traitement', router);
