52ee6339234ea827bd43a81cb4fec03aa7b5c343
BadreddineZatout <hb_zatout@esi.dz>
2021-06-17 15:21:44 +0100

    create doctor and patient with post requests

--- App/controllers/Patient.controller.js
+++ App/controllers/Patient.controller.js
@@ -1,6 +1,27 @@
 const db = require("../models");
 const Patient = db.patients;
 
+exports.create = (req, res) => {
+  // Validate request
+  if (!req.body.name) {
+    res.status(400).send({
+      message: "Content can not be empty!"
+    });
+    return;
+  }
+
+  // Save Patient in the database
+  Patient.create(req.body)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while creating the Tutorial."
+      });
+    });
+};
 exports.findAuth = (req, res) => {
     const tel = req.body.tel;
     const pdw = req.body.pdw;
@@ -8,8 +29,7 @@ exports.findAuth = (req, res) => {
         where: {
             tel: tel,
             pdw: pdw
-        },
-        attributes: ['id', 'name', 'tel']
+        }
      })
       .then(data => {
         if (data.length > 0){
@@ -33,7 +53,7 @@ exports.findAuth = (req, res) => {
   exports.findOne = (req, res) => {
     const id = req.params.id;
   
-    Patient.findByPk(id, {attributes: ['id', 'name', 'tel']})
+    Patient.findByPk(id)
       .then(data => {
         res.send(data);
       })
