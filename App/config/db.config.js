a9030c4e2b5cd3d719bc7bcd1afd468d5223e676
BadreddineZatout <hb_zatout@esi.dz>
2021-07-02 16:01:20 +0100

    heroku database

--- App/config/db.config.js
+++ App/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "sil1",
-    PASSWORD: "sil1",
-    DB: "AskDoc",
+    HOST: "ec2-54-216-48-43.eu-west-1.compute.amazonaws.com",
+    USER: "maeaopbuxvojcs",
+    PASSWORD: "e91654ce1894e0cd5385ec68023f76c2048a76c8319b67d45463a8ff92ff932a",
+    DB: "d4jsakgmtgkkb7",
     dialect: "postgres",
     pool: {
       max: 5,
