33423e55ad65beacd5077b49627305278899b821
BadreddineZatout <hb_zatout@esi.dz>
2021-07-05 00:41:19 +0100

    fix

--- App/models/Conseil.model.js
+++ App/models/Conseil.model.js
@@ -2,11 +2,9 @@ module.exports = (sequelize, Sequelize) => {
     const Conseil = sequelize.define("conseil", {
         iddoc: {
         type: Sequelize.INTEGER,
-        primaryKey: true
       },
       idpat: {
         type: Sequelize.INTEGER,
-        primaryKey: true
       },
       text: {
         type: Sequelize.STRING
